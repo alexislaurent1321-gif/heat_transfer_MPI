6ebca62100083019709cdf422caa309ceb8682e6
alexislaurent1321-gif <alexis.laurent1321@gmail.com>
2026-01-13 14:50:44 +0100

    structuration

---
+++ run.bat
@@ -0,0 +1,26 @@
+@echo off
+
+:: Compilation avec mpic++
+mpic++ -std=c++20 -o test.exe main.cpp solve.cpp
+
+:: Exécution avec mpirun pour 1, 2 et 4 processus
+mpiexec --use-hwthread-cpus -n 1 test.exe
+mpiexec --use-hwthread-cpus -n 2 test.exe
+mpiexec --use-hwthread-cpus -n 4 test.exe
+
+:: Exécution des scripts Python
+python plot_T.py
+python plot_mpi.py
+
+:: Suppression des fichiers
+del test.exe
+
+del mpi_results_1.txt
+del mpi_results_2.txt
+del mpi_results_4.txt
+
+del T_data_0.txt
+del T_data_1.txt
+del T_data_2.txt
+del T_data_3.txt
+del T_data.txt
