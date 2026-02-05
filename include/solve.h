76229b3b21d606321165b17774255ecbf4c055e0
Alexis Laurent <alexis.laurent1321@gmail.com>
2026-02-05 15:10:03 +0100

    1D vectors

--- include/solve.h
+++ include/solve.h
@@ -4,26 +4,25 @@
 #ifndef SOLVE_H
 #define SOLVE_H
 
-using namespace std;
+// Filling the T matrix with the exact solution
+void T_ex(std::vector<double>& T, double t, int Nx, int Ny, int rank, int nprocs); 
 
-void T_ex(vector<vector<double>>& T, double t, int Nx, int Ny, int rank, int nprocs); // Filling the T matrix with the exact solution
-
-double T_ex(double t, double x, double y); // Exact solution of T evaluated at a point
+// Exact solution of T evaluated at a point
+double T_ex(double t, double x, double y); 
 
 // Calculation of the infinite standard error between T and T_ex
-double error_T(vector<vector<double>> T, int Nx, int Ny, int rank, int nprocs);
+double error_T(const std::vector<double>& T, int Nx, int Ny, int rank, int nprocs);
 
-double max_T(vector<vector<double>> T, int Nx, int Ny, int rank, int nprocs);
-
-
+// Calculation of the maximum value of T
+double max_T(const std::vector<double>& T, int Nx, int Ny, int rank, int nprocs);
 
 /*
 Functions implementing the parallelized finite difference method
 */
 
 // Update T using finite differences over a time iteration
-void updateT(const vector<vector<double>>& T, vector<vector<double>>& T_plus, int Nx, int Ny, int rank, int nprocs);   
-
+void updateT(const std::vector<double>& T, std::vector<double>& T_plus, int Nx, int Ny);   
+  
 // Resolution of T with MPI
 double solve(int N, int rank, int nprocs, double *err_max, double *Tmax);
 
--- src/solve.cpp
+++ src/solve.cpp
@@ -14,52 +14,60 @@ extern Param p; // Importing settings
 Filling T at time t
 Block division on x
 */
-void T_ex(vector<vector<double>>& T, double t, int Nx, int Ny, int rank, int nprocs){
-    double x, y;
-    for(int i=0; i<Nx; ++i){
-        x = p.xmin + (rank*Nx+i)*(p.xmax-p.xmin)/(Nx*nprocs);
-        for(int j=0; j<Ny; ++j){
-            y = p.ymin + j*(p.ymax-p.ymin)/Ny;
-            T[i][j] = p.Tmax/(1+2*t*p.kappa/pow(p.sigma,2))*exp((-pow(x,2)-pow(y,2))/(2*pow(p.sigma,2)+4*t*p.kappa));
+void T_ex(std::vector<double>& T, double t, int Nx_local, int Ny, int rank, int nprocs){
+    
+    for(int i=0; i < Nx_local; ++i){
+        double x = p.xmin + (rank * (Nx_local - 2) + (i - 1)) * p.dx;
+        
+        for(int j=0; j < Ny; ++j){
+            double y = p.ymin + j * p.dy;
+            T[i * Ny + j] = T_ex(t, x, y);
         }
     }
 }
 
 // Exact solution of T evaluated at a point
 double T_ex(double t, double x, double y){
-    return p.Tmax/(1+2*t*p.kappa/pow(p.sigma,2))*exp((-pow(x,2)-pow(y,2))/(2*pow(p.sigma,2)+4*t*p.kappa));
+    double denom = 2 * pow(p.sigma, 2) + 4 * t * p.kappa;
+    double ampl = p.Tmax / (1 + 2 * t * p.kappa / pow(p.sigma, 2));
+    return ampl * exp((-pow(x, 2) - pow(y, 2)) / denom);
 }
 
 // Calculation of the infinite standard error between T and T_ex
-double error_T(vector<vector<double>> T, int Nx, int Ny, int rank, int nprocs){
-    double err = 0;     // max arror
-    double err_ij;      // error evaluated at a point
-    double x, y;        // definition of x and y based on indices i and j
-    for(int i=1; i<Nx-1; i++){
-        x = p.xmin + (rank*Nx+i)*(p.xmax-p.xmin)/(Nx*nprocs);
-        for(int j=0; j<Ny; j++){
-            y = p.ymin + j*(p.ymax-p.ymin)/Ny;
-            err_ij = fabs(T[i][j]-T_ex(p.t_final,x,y));
-            if(err<err_ij) err=err_ij;
+double error_T(const std::vector<double>& T, int Nx, int Ny, int rank, int nprocs){
+    
+    double err = 0;
+    
+    for(int i=1; i < Nx-1; i++){
+        double x = p.xmin + (rank * (Nx - 2) + (i - 1)) * p.dx;
+        
+        for(int j=0; j < Ny; j++){
+            double y = p.ymin + j * p.dy;
+            double err_ij = std::abs(T[i * Ny + j] - T_ex(p.t_final, x, y));
+            if(err < err_ij) err = err_ij;
         }
     }
-
+    
     return err;
 }
 
 // Calculation of the infinite standard error between T and T_ex
-double max_T(vector<vector<double>> T, int Nx, int Ny, int rank, int nprocs){
+double max_T(std::vector<double>& T, int Nx, int Ny, int rank, int nprocs){
+
     double Tmax = 0;
     double T_ij;    // error evaluated at a point
     double x, y;    // definition of x and y based on indices i and j
+
     for(int i=1; i<Nx-1; i++){
         x = p.xmin + (rank*Nx+i)*(p.xmax-p.xmin)/(Nx*nprocs);
+
         for(int j=0; j<Ny; j++){
             y = p.ymin + j*(p.ymax-p.ymin)/Ny;
             T_ij = T_ex(p.t_final,x,y);
             if(fabs(Tmax)<fabs(T_ij)) Tmax=T_ij;
         }
     }
+
     return Tmax;
 }
 
@@ -71,12 +79,17 @@ Numerical resolution functions
 */
 
 // Update T using finite differences
-void updateT(const vector<vector<double>>& T, vector<vector<double>>& T_plus, int Nx, int Ny, int rank, int nprocs) {
-    for(int i=1; i<Nx-1; ++i) {
-        for(int j=1; j<Ny-1; ++j) {
-            T_plus[i][j] = T[i][j] + p.kappa * p.dt * (
-                (T[i+1][j] - 2*T[i][j] + T[i-1][j]) / (p.dx*p.dx) +
-                (T[i][j+1] - 2*T[i][j] + T[i][j-1]) / (p.dy*p.dy));
+void updateT(const std::vector<double>& T, std::vector<double>& T_plus, int Nx, int Ny) {
+    int index;
+    for(int i=1; i < Nx-1; ++i) {
+        
+        for(int j=1; j < Ny-1; ++j) {
+            index = i * Ny + j;
+            // Correction des parenthèses d'indexation
+            T_plus[index] = T[index] + p.kappa * p.dt * (
+                (T[(i+1)*Ny + j] - 2*T[index] + T[(i-1)*Ny + j]) / (p.dx*p.dx) +
+                (T[index + 1] - 2*T[index] + T[index - 1]) / (p.dy*p.dy)
+            );
         }
     }   
 }
@@ -84,55 +97,47 @@ void updateT(const vector<vector<double>>& T, vector<vector<double>>& T_plus, in
 
 // Function to execute the Jacobi method with MPI
 double solve(int N, int rank, int nprocs, double *err_max, double *Tmax) {
+    // N_local inclut 2 lignes fantômes (sauf bords extrêmes, mais simplifié ici)
+    int Nx_local = N / nprocs + 2;
+    int Ny = N;
 
-    int N_local = 0;
-    if(nprocs==1) 
-        N_local = N;
-    else
-        N_local = N / nprocs + 2; // Number of local lines (includes ghost lines)
+    std::vector<double> T(Nx_local * Ny, 0.0);
+    std::vector<double> T_plus(Nx_local * Ny, 0.0);
 
-    // Creation and filling of block T with the solution at initial time
-    vector<vector<double>> T(N_local,vector<double>(N,0));
-    vector<vector<double>> T_plus(N_local,vector<double>(N,0));
-    T_ex(T,0,N_local,N,rank,nprocs);
-    T_ex(T_plus,0,N_local,N,rank,nprocs);
+    T_ex(T, 0, Nx_local, Ny, rank, nprocs);
+    T_plus = T;
 
     double start_time = MPI_Wtime();
 
-    for (int iter = 0; iter <= p.Nt; ++iter) {
-        // Exchange border values with neighboring threads
+    for (int iter = 0; iter < p.Nt; ++iter) {
+        // Échanges des Ghost Cells (Bloquants mais sûrs pour débuter)
         if (rank > 0) {
-            MPI_Send(&T[1][0], N, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD);
-            MPI_Recv(&T[0][0], N, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            MPI_Sendrecv(&T[Ny], Ny, MPI_DOUBLE, rank-1, 0, 
+                         &T[0], Ny, MPI_DOUBLE, rank-1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
         if (rank < nprocs-1) {
-            MPI_Recv(&T[N_local-1][0], N, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            MPI_Send(&T[N_local-2][0], N, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD);
+            MPI_Sendrecv(&T[(Nx_local-2)*Ny], Ny, MPI_DOUBLE, rank+1, 0, 
+                         &T[(Nx_local-1)*Ny], Ny, MPI_DOUBLE, rank+1, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
         }
 
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // Calculation of the update of T
-        updateT(T, T_plus, N_local, N, rank, nprocs);
+        updateT(T, T_plus, Nx_local, Ny);
         T.swap(T_plus);
     }
+    
     double end_time = MPI_Wtime();
 
-    // Printing the table T
-    std::vector<ofstream> T_file(nprocs);
-    T_file[rank].open(fmt::format("T_data_{}.txt", rank));
-    for(int i=1; i<N_local-1; i++){
-        T_file[rank] << "\n";
-        for(int j=0; j<N; j++){
-            T_file[rank]<< T[i][j] << " ";
+    // Sauvegarde propre
+    std::ofstream file(fmt::format("T_data_{}.txt", rank));
+    for(int i=1; i < Nx_local-1; i++){
+        for(int j=0; j < Ny; j++){
+            file << T[i * Ny + j] << (j == Ny-1 ? "" : " ");
         }
+        file << "\n";
     }
-    T_file[rank].close();
+    file.close();
 
-    
-    // Calculation of the inf error
-    *err_max = error_T(T_plus, N_local, N, rank, nprocs);
-    *Tmax = max_T(T_plus, N_local, N, rank, nprocs);
+    *err_max = error_T(T, Nx_local, Ny, rank, nprocs);
+    *Tmax = max_T(T, Nx_local, Ny, rank, nprocs);
 
-    return end_time - start_time; // compute time
+    return end_time - start_time;
 }
